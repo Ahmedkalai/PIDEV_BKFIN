4c37384ac5c0d74bf83c4af3a49d721dd7006136
lzemna <47740361+lzemna@users.noreply.github.com>
2022-04-06 22:20:21 +0100

    on a 2 types de comptes bancaires : un compte épargne avec un taux d'interet et compte courant pour les simples transactions bancaires

--- src/main/java/com/BKFIN/entities/Account.java
+++ src/main/java/com/BKFIN/entities/Account.java
@@ -32,7 +32,9 @@ public class Account implements Serializable {
 	@Column(name="Rib")
 	private String Rib ; 
 	private float sold ;
+//ces deux attributs nous serons utiles pour le calcul du taux d'interet 
 	private float interest;
+//cette variable s'incremente à chaque fois que le cron programmé s'execute 
 	private int index_interest;
 	//@DateTimeFormat(pattern = "yyyy-MM-dd")
 	//@Temporal (TemporalType.DATE)
