fb9547606c555d55533ccb1c849078a4ca4013fc
YassinBenZekri <yassine.benzekri@esprit.tn>
2022-04-06 22:42:41 +0200

    admin add

--- src/main/java/com/BKFIN/entities/Admin.java
+++ src/main/java/com/BKFIN/entities/Admin.java
@@ -1,28 +1,21 @@
 package com.BKFIN.entities;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.Date;
 import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
 @Entity
-public class Admin implements Serializable,UserDetails {
+public class Admin implements Serializable {
 	
 	@Id
 	 @Column(name ="idAdmin")
@@ -34,9 +27,6 @@ public class Admin implements Serializable,UserDetails {
 	private Date birthDay ;
 	private Long phoneNum;
 	private String email;
-	private String password;
-	@ManyToMany(cascade = CascadeType.PERSIST, fetch = FetchType.EAGER)
-	 private Set<Role> roles;
 	@OneToMany( mappedBy="admin")
 	private Set<Agent> agent;
 	public long getIdAdmin() {
@@ -81,68 +71,6 @@ public class Admin implements Serializable,UserDetails {
 	public void setAgent(Set<Agent> agent) {
 		this.agent = agent;
 	}
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return this.password;
-	}
-	@Override
-	public String getUsername() {
-		// TODO Auto-generated method stub
-		return this.email;
-	}
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-	public Set<Role> getRoles() {
-		return roles;
-	}
-	public void setRoles(Set<Role> roles) {
-		this.roles = roles;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public Admin(long idAdmin, String name, String secondName, Date birthDay, Long phoneNum, String email,
-			String password, Set<Role> roles, Set<Agent> agent) {
-		super();
-		this.idAdmin = idAdmin;
-		this.name = name;
-		this.secondName = secondName;
-		this.birthDay = birthDay;
-		this.phoneNum = phoneNum;
-		this.email = email;
-		this.password = password;
-		this.roles = roles;
-		this.agent = agent;
-	}
-	public Admin() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
 	
 	
 }
